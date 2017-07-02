f122c68bdd5b12012b1bde1c858b5433c36ad92e
luisalduucin <lalducin@nearsoft.com>
2017-07-02 14:36:52 -0500

    Renamed imaga to image_utils

--- Stylex_TF_Core/image_utils.py
+++ Stylex_TF_Core/image_utils.py
@@ -10,10 +10,8 @@ def load(filename):
 
 
 def save(image, filename):
-    # Aseguramos que los valores de los pixeles esten dentro del rango de 0 a 255.
     image = np.clip(image, 0.0, 255.0)
 
-    # Convertimos a bytes.
     image = image.astype(np.uint8)
 
     with open(filename, 'wb') as file:
@@ -27,43 +25,26 @@ def plot_single(image):
 
 
 def plot_results(content_image, style_image, mixed_image):
-    # Create figure with sub-plots.
     fig, axes = plt.subplots(1, 3, figsize=(10, 10))
 
-    # Adjust vertical spacing.
     fig.subplots_adjust(hspace=0.1, wspace=0.1)
 
-    # Use interpolation to smooth pixels?
-    smooth = True
+    interpolation = 'sinc'
 
-    # Interpolation type.
-    if smooth:
-        interpolation = 'sinc'
-    else:
-        interpolation = 'nearest'
-
-    # Plot the content-image.
-    # Note that the pixel-values are normalized to
-    # the [0.0, 1.0] range by dividing with 255.
     ax = axes.flat[0]
     ax.imshow(content_image / 255.0, interpolation=interpolation)
     ax.set_xlabel("Content")
 
-    # Plot the mixed-image.
     ax = axes.flat[1]
     ax.imshow(mixed_image / 255.0, interpolation=interpolation)
     ax.set_xlabel("Mixed")
 
-    # Plot the style-image
     ax = axes.flat[2]
     ax.imshow(style_image / 255.0, interpolation=interpolation)
     ax.set_xlabel("Style")
 
-    # Remove ticks from all the plots.
     for ax in axes.flat:
         ax.set_xticks([])
         ax.set_yticks([])
 
-    # Ensure the plot is shown correctly with multiple plots
-    # in a single Notebook cell.
     plt.show()
